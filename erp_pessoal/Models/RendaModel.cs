b070d1cdb292ed0eed5d3708b79acea8261c717a
João Butzen <butzenjvlb@gmail.com>
2026-04-28 13:13:06 -0300

    fix/ Adjust rendas to receive models

---
+++ erp_pessoal/Models/RendaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace erp_pessoal.Models
+{
+    public class RendaModel
+    {
+        public string descricao { get; set; }
+        public decimal vlr_min { get; set; }
+        public decimal vlr_max { get; set; }
+        public DateTime data { get; set; }
+    }
+}
---
+++ erp_pessoal/Models/RendaUpdateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace erp_pessoal.Models
+{
+    public class RendaUpdateModel
+    {
+        public int id_renda {  get; set; }
+        public string descricao { get; set; }
+        public decimal vlr_min { get; set; }
+        public decimal vlr_max { get; set; }
+        public DateTime data { get; set; }
+    }
+}
